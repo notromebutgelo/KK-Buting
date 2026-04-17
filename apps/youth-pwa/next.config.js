922a9d0e15a2b666d8de06f8bc518f393e9a7074
notromebutgelo <jeromeangeloalison@gmail.com>
2026-04-17 22:21:47 +0800

    fix login speed of google sign-in

--- apps/youth-pwa/next.config.js
+++ apps/youth-pwa/next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['firebasestorage.googleapis.com'] },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ]
+  },
 }
 module.exports = nextConfig
